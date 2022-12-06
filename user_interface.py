2a932ef7ca50b2e5271dc3e0c0dd5d3edc44aba6
BFitzy96 <brughafitzpatrick@gmail.com>
2022-12-06 21:12:30 +0000

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -5,8 +5,10 @@ from inspect import getmembers, isfunction
 options = getmembers(biomedRecords, isfunction)
 options = [options[opt][0] for opt, _ in enumerate(options)]
 
+print('\t\t\tPlease an option below:')
+
 for i in range(1, len(options)-1, 2):
     print(f'\t\t\t{options[i]}, {options[i+1]}')
 
 
-choice = input('\t\tPlease choose one of the inputs above: \n\t\t\t')
+choice = input('\t\t\t')
